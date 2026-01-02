53ed7c69cd8aaeba95c6a369c2abe331cabdef45
junghyungo <pp90971658@gmail.com>
2026-01-02 23:32:45 +0900

    19: 색상, 문자열 리소스 추출

--- app/src/main/java/com/example/bfdream_android/ui/theme/Color.kt
+++ app/src/main/java/com/example/bfdream_android/ui/theme/Color.kt
@@ -2,6 +2,7 @@ package com.example.bfdream_android.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// 기본 자동 생성 컬러
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +10,27 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// 맘편한 이동 컬러
+val pr_LavenderPurple = Color(0xFFA1ACF9)
+val pr_White = Color(0xFFFFFFFF)
+val pr_PeriwinkleBlue = Color(0xFF4E71FF)
+
+val tx_Black = Color(0xFF000000)
+
+val se_SkyBlue = Color(0xFF8DD8FF)
+val se_MintBlue = Color(0xFFBBFBFF)
+val se_OffWhite = Color(0xFFF9F7F7)
+val se_PaleGray = Color(0xFFDBE2EF)
+val se_OceanBlue = Color(0xFF3F72AF)
+val se_MidnightNavy = Color(0xFF112D4E)
+
+val bl_TrunkBus_NBus = Color(0xFF0066CC)
+val bl_FeederBus_TownBus = Color(0xFF00A651)
+val bl_CircularBus = Color(0xFFFFD320)
+val bl_WideAreaBus = Color(0xFFE60012)
+val bl_AirportBus = Color(0xFFC79B00)
+
+val state_Crowded = Color(0xFFFF0000)
+val state_Normal = Color(0xFFFFA500)
+val state_Comfort = Color(0xFF00AA00)
